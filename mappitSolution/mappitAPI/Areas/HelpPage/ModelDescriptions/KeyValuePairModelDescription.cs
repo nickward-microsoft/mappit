50720ae7345cccfd202a1f225c0c88ae201eca08
nickward-microsoft <nickward@microsoft.com>
2016-04-07 15:09:26 +1000

    Initial out-of-the-box web api for the mappitapi

---
+++ mappitSolution/mappitAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace mappitAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
